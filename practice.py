ea64bb453a79025408031ad7433bd7119dbf5fb4
Khizar Qamar <khizarqamar345@gmail.com>
2026-03-31 14:45:31 +0500

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,147 @@
+#print name or age.
+#name="Khizar Qamar"
+#age=16
+#print(name,age)
+#also write as:
+#print(name)
+#print(age)
+#--------------
+#sum of two numbers
+#a=10
+#b=20
+#print(a+b)
+#-----------------
+#cheak even or odd numbers.
+#num=int(input("Enter a number"))
+#if num %2==0:
+#    print("NUmber is even")
+#else:
+#    print("NUmber is odd")    
+#-------------------
+#sum or multiply.
+#num1=int(input("Enter 1st number"))
+#num2=int(input("Enter 2nd Number"))
+#sum_num=num1+num2
+#multiply_num=num1*num2
+#print(sum_num)
+#print(multiply_num)
+#--------------------
+#square of number.
+#num=int(input("Enter a number"))
+#print(num*num)
+#------------------
+#Age adult or minor
+#age=int(input("Enter a number"))
+#if age>=18:
+#    print("Adult")
+#else:
+#    print("Minor")    
+#------------------------
+#marks pass or fail
+#marks=int(input("Enter a number"))
+#if marks>=90:
+#    print("Grade A+")
+#elif marks>=80:
+#    print("Grade A")
+#elif marks>=70:
+#    print("Grade B")
+#elif marks>60:
+#    print("Grade C")
+#else:
+#   print("Fail")            
+#-------------------
+#loop to print 1 to 10
+#for i in range(1,11):
+#     print(i)
+#-----------------------
+#print the table of numbers.
+#num=int(input("Enter a number"))
+#for i in range (1,11):
+#    print(num,"*", i,"=",num*i)
+#---------------------------
+#print even numbers from 1 to 20.
+#for i in range(1,21):
+#    if i%2==0:
+#        print(i)
+#--------------------------
+#🟢 Basic (Variables + Input)
+#name=input("Enter your name:")
+#city=input("Enetr your city name:")
+#print(name)
+#print(city)
+#--------------------------
+#differnce
+#num1=int(input("Enter the 1st number:"))
+#num2=int(input("enter the 2nd number:"))
+#print(num1-num2)
+#-----------------------------
+#cube
+#num=int(input("Enter a number:"))
+#print(num**3)
+#------------------------------
+#🟡 Data Types
+#cheaking type
+#a="Khizar" #string
+#b=10       #int
+#c=True     #bool
+#d=12.5     #float
+#print(type(a))
+#print(type(b))
+#print(type(c))
+#print(type(d))
+#----------------------------------
+#cheaking type from user inut
+#user_input=input("write anything:")
+#if user_input.isdigit():
+#    print("This is Int")
+#elif "." in user_input:
+#    print("This is Float")
+#else:
+#    print("This is Str ")    
+#--------------------------------
+#🔵 Conditions
+#To print even or odd from user input
+#num=int(input("Enter a number:"))
+#if num %2==0:
+#    print("This is even")
+  
+#else:
+#    print("This is odd")    
+#---------------------------------
+#cheaking positive negative or zero
+#num=int(input("Enter  number:"))
+#if num>=1:
+#    print("This is positive")
+#elif num<0:
+#    print("This is negative")
+#else:
+#    print("This is zero")        
+#-------------------------------
+#🟣 Loops
+#print 1 to 50 even numbers
+#for i in range(1,51):
+#    if i%2==0:
+#        print(i)
+#-----------------------------
+#taking input from user and print table
+#num=int(input("Enter a  number"))
+#for i in range(1,11):
+#    print(num ,"*",i,"=",num*i)
+#---------------------------
+#🔴 Mixed
+#finding largest one number
+#num1=int(input("Enter 1st number:"))
+#num2=int(input("Enter 2nd number:"))
+#num3=int(input("Enter 3rd number:"))
+#if num1>num2:
+#    print(num1,"Num1 is largest")
+#elif num2>num3:
+#    print(num2,"Num2 is largest")  
+#else:
+#    print(num3,"Num3 is largest")      
+#------------------------------------------
+#print 1 to 100 numbers (not those whose divde by 3)
+#for i in range(1,101):
+#    if i%3!=0:
+#        print(i)
+#----------------------------------------------------------------
